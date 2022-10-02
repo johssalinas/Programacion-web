b40456e7ad423d54a6af936268044d27ea90a02c
JohsB <brayansalinas2345@gmail.com>
2022-10-02 12:57:01 -0500

    Primeros pasos en vue.js

---
+++ Programacion Web/segundo corte/Trabajo22octubre/trabajo22octubre/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{texto}}</h2>
+  </div>
+  <div style="background: red;"><miComponente/></div>
+</template>
+
+<script>
+
+import miComponente from './miComponente.vue';
+export default {
+  name: 'HelloWorld',
+  components: {
+    miComponente,
+},
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      texto: "Hola mundo desde componente!!"
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ Programacion Web/segundo corte/Trabajo22octubre/trabajo22octubre/src/components/miComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="mi-componente">
+        <hr/>
+            <h1>Este es mi componente</h1>
+            <h2>{{subtitulo}}</h2>
+        <hr/>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'mi-componente',
+        data(){
+            return{
+                subtitulo: 'Soy el subtitulo de mi componente'
+            }
+        }
+    }
+</script>
