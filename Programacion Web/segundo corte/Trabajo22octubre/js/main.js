b1aec45c20fc5df3ba715bb16e8536194813296e
YhonOchoa <105899541+YhonJ8a@users.noreply.github.com>
2022-10-08 12:07:02 -0500

    IMPLEMENTACION de los metodos para cada ejercicio empesande desde el ejercicion nro 11

---
+++ Programacion Web/segundo corte/Trabajo22octubre/js/main.js
@@ -0,0 +1,70 @@
+
+function ejercio1(){}
+
+function ejercio2(){}
+
+function ejercio3(){}
+
+function ejercio4(){}
+
+function ejercio5(){}
+
+function ejercio6(){}
+
+function ejercio7(){}
+
+function ejercio8(){}
+
+function ejercio9(){}
+
+function ejercio10(){}
+
+function ejercio11(){
+    var num;
+    var mayor = 0 ;
+    var menor = 0;
+    for (var i = 0; i< 4; i++){
+        num = prompt('Ingrese un  numero');
+        if(num > mayor) { mayor = num;}
+        if(num > menor){menor = num;}
+    }
+    
+}
+
+function ejercio12(){}
+
+function ejercio13(){}
+
+function ejercio14(){}
+
+function ejercio15(){}
+
+function ejercio16(){}
+
+function ejercio17(){}
+
+function ejercio18(){}
+
+function ejercio19(){}
+
+function ejercio20(){}
+
+function ejercio21(){}
+
+function ejercio22(){}
+
+function ejercio23(){}
+
+function ejercio24(){}
+
+function ejercio25(){}
+
+function ejercio26(){}
+
+function ejercio27(){}
+
+function ejercio28(){}
+
+function ejercio29(){}
+
+function ejercio30(){}
